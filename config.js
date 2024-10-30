6e914f5c0b861a481cda438a8949fa8fec132bf1
JadenMordark <thanhsontran420@gmail.com>
2024-10-31 02:19:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,21 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    titleWeb: "lời xin lỗi muộn",
+    introTitle: 'Em iu ơiii',
+    introDesc: `Anh đã sai rồi ạ
+    Anh yêu em nhưng đã ghen mù quáng
+    Lạc vào những suy nghĩ sai lệch
+    Nhưng đằng sau chữ yêu là thương
+    Anh thương em Trần Gia Nghi`,
+    btnIntro: 'Anh vụng về nhỉ :3',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    btnYes: 'Nhớ lắm <33',
+    btnNo: 'À không...',
+    question: 'Anh là một người hài hước, thấy cả ngàn nụ cười nhưng chỉ nụ cười em làm anh xao xuyến khôn nguôi',
+    btnReply: 'Gửi cho baby <3',
+    reply: 'Yêu thì hãy tha thứ cho chàng sư tử vụng về này nhaaa <3',
+    mess: 'Anh năn nỉ mà 🥰. Yêu en nhiều nhiều 😘😘',
+    messDesc: 'Tối mai, hâm nóng tình cảm nha. 😳',
+    btnAccept: 'Đừng bùn nữa nhaaaa ~.~',
+    messLink: 'https://www.messenger.com/e2ee/t/7851548184924414' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
